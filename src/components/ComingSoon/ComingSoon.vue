1237c9db9a5dee0613b40b8bed11041795e5e82e
Redwolf <benedicte.pancheri@gmail.com>
2020-10-30 09:02:46 +0100

    Coming Soonpage

---
+++ src/components/ComingSoon/ComingSoon.vue
@@ -0,0 +1,13 @@
+<template>
+    <h2>Coming Soon</h2>
+</template>
+
+<script>
+    export default {
+        name: "ComingSoon"
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/Home/Home.vue
@@ -0,0 +1,33 @@
+<template>
+    <section class="section-home">
+        <div class="home-title m">
+            <h1 class="-darkRed">RedWolf</h1>
+            <h3 class="-darkRed">Digital Agency</h3>
+            <ComingSoon class="-darkRed"></ComingSoon>
+        </div>
+        <div class="home-bg">
+            <div class="home-title m -white" >
+                <h1 class="-white">RedWolf</h1>
+                <h3 class="-white">Digital Agency</h3>
+                <ComingSoon></ComingSoon>
+            </div>
+        </div>
+    </section>
+</template>
+
+<script>
+
+    import ComingSoon from "@/components/ComingSoon/ComingSoon";
+    export default {
+        name: "Home",
+        components: {
+            ComingSoon
+        },
+
+
+    }
+</script>
+
+<style scoped>
+
+</style>
